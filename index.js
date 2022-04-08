2213d0015daafc3d5d2cd60d02a550fceb55a2fd
Jessica <jess.newman19@gmail.com>
2022-04-08 16:58:21 -0400

    commit tree 1f187c70603cf098129179877962e79ab209625c

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Your code here
+// Grabbing dodger and saving a reference to it
+const dodger = document.getElementById("dodger"); 
+dodger.style.backgroundColor = "FF69B4"
+
+//First attempt
+// document.addEventListener("keydown", (event) => 
+//   if (event.key === "ArrowLeft") { //Does the key property of the event object (keydown) have the value "ArrowLeft"?
+//     const leftNumbers = dodger.style.left.replace("px", ""); 
+//     const left = parseInt(leftNumbers, 10)
+
+//     dodger.style.left = `${left-1}px` //Everytime left is clicked, decrease the px by 1px.
+//   }
+// })
+
+//Second attempt
+const moveDodgerLeft = () => { 
+  const leftNumbers = dodger.style.left.replace("px", ""); 
+  const left = parseInt(leftNumbers, 10); 
+  if (left > 0 ) { //Move Dodger left until px on the left = 0
+    dodger.style.left = `${left-1}px`
+  }
+}
+
+document.addEventListener("keydown", (e) => { 
+  if (e.key === "ArrowLeft") { //If the keydown object key = "ArrowLeft"
+    moveDodgerLeft()
+  }
+})
